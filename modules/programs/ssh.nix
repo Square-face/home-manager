f8cff9eef783a03a5d16a04ed28387cfe50ab8bd
Linus Michelsson <linus@sq8.dev>
2026-03-11 17:03:40 +0100

    feat: ssh config

---
+++ modules/programs/ssh.nix
@@ -0,0 +1,49 @@
+{pkgs, ...}: {
+    programs.ssh.enable = true;
+    programs.ssh.package = pkgs.openssh_hpnWithKerberos;
+    programs.ssh.extraConfig = ''
+    CanonicalizeHostname yes
+    CanonicalDomains dh3.ludd.ltu.se t1.ludd.ltu.se ludd.ltu.se
+    
+    Host *.ltu.se
+      GSSAPIAuthentication yes
+      GSSAPIDelegateCredentials yes
+    '';
+
+    programs.ssh.matchBlocks = {
+        "*" = {
+            forwardAgent = false;
+            addKeysToAgent = "no";
+            compression = false;
+            serverAliveInterval = 0;
+            serverAliveCountMax = 3;
+            hashKnownHosts = false;
+            userKnownHostsFile = "~/.ssh/known_hosts";
+            controlMaster = "no";
+            controlPath = "~/.ssh/master-%r@%n:%p";
+            controlPersist = "no";
+        };
+        "markus" = {
+            hostname = "10.2.0.1";
+            user = "root";
+            port = 22;
+        };
+        "sitbix" = {
+            hostname = "10.2.1.1";
+            user = "root";
+            port = 22;
+        };
+        "frank" = {
+            hostname = "10.2.2.1";
+            user = "root";
+            port = 22;
+        };
+        "shrexbox" = {
+            hostname = "10.2.100.1";
+            user = "sq8";
+            port = 22;
+        };
+    };
+
+    programs.ssh.enableDefaultConfig = false;
+}
