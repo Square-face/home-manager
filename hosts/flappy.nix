2c6ebb400fab166039118d26eed441ce359aaa58
Linus Michelsson <linus@sq8.dev>
2026-04-17 06:53:11 +0200

    breaking(firefox): added firefox.enable option to enable firefox

--- hosts/flappy.nix
+++ hosts/flappy.nix
@@ -24,6 +24,7 @@ in {
   };
 
   graphical.enable = true;
+  firefox.enable = true;
 
   alacritty.enable = true;
 
--- modules/graphical/firefox.nix
+++ modules/graphical/firefox.nix
@@ -1,53 +1,74 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }: {
+  options.firefox.enable = lib.mkEnableOption "Enable firefox";
+  config = lib.mkIf (config.firefox.enable && config.graphical.enable) {
+    programs.firefox.enable = true;
+    programs.firefox.package = pkgs.librewolf;
+    programs.firefox.policies = {
 
-# WELCOME TO THE WORLD OF MINDFUCKERY SUCKAAAA!
-
-{
-  programs.firefox = {
-    enable = true;
-    # Refer to https://mozilla.github.io/policy-templates or `about:policies#documentation` in firefox
-    package = pkgs.librewolf;
-    policies = {
-      AppAutoUpdate = false; # Disable automatic application update
-      BackgroundAppUpdate = false; # Disable automatic application update in the background, when the application is not running.
-      DisableBuiltinPDFViewer = true; # Considered a security liability
-      DisableFirefoxStudies = true;
-      DisableFirefoxAccounts = true; # Disable Firefox Sync
-      DisableFirefoxScreenshots = true; # No screenshots?
-      DisableForgetButton = true; # Thing that can wipe history for X time, handled differently
-      DisableMasterPasswordCreation = true; # To be determined how to handle master password
-      DisableProfileImport = true; # Purity enforcement: Only allow nix-defined profiles
-      DisableProfileRefresh = true; # Disable the Refresh Firefox button on about:support and support.mozilla.org
-      DisableSetDesktopBackground = true; # Remove the “Set As Desktop Background…” menuitem when right clicking on an image, because Nix is the only thing that can manage the backgroud
-      DisplayMenuBar = "default-off";
-      DisablePocket = true;
-      DisableTelemetry = true;
-      DisableFormHistory = true;
-      DisablePasswordReveal = true;
-      DontCheckDefaultBrowser = true; # Stop being attention whore
+      # Fine
       HardwareAcceleration = true;
-      OfferToSaveLogins = false; # Managed by Bitwarden instead
-      EnableTrackingProtection = {
-        Value = true;
+      PictureInPicture = {
+        Enabled = true;
         Locked = true;
-        Cryptomining = true;
-        Fingerprinting = true;
-        EmailTracking = true;
-        # Exceptions = ["https://example.com"]
       };
       EncryptedMediaExtensions = {
         Enabled = true;
         Locked = true;
       };
+
+      # Imagine having a imperative browser
+      AppAutoUpdate = false; 
+      BackgroundAppUpdate = false; 
+
+      # Attention whore
+      DontCheckDefaultBrowser = true; 
+      UserMessaging = {
+        ExtensionRecommendations = false;
+        FeatureRecommendations = false;
+        Locked = true;
+        MoreFromMozilla = false;
+        SkipOnboarding = true;
+        UrlbarInterventions = false;
+        WhatsNew = false;
+      };
+
+      DisableFirefoxStudies = true;
+      DisableFirefoxAccounts = true; 
+      DisableFirefoxScreenshots = true; 
+      DisableMasterPasswordCreation = true; 
+      DisableProfileImport = true; 
+      DisableProfileRefresh = true; 
+      DisableSetDesktopBackground = true; 
+      DisplayMenuBar = "default-off";
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DisableFormHistory = true;
+      DisablePasswordReveal = true;
+      SearchSuggestEnabled = false;
+
+      # Declutter
+      ShowHomeButton = false;
+      NoDefaultBookmarks = true;
+      DisableForgetButton = true; 
+
+      # Bitwarden does it better
+      OfferToSaveLogins = false; 
+      PasswordManagerEnabled = false; # Managed by KeepAss
+
+      # just use evince bro
+      DisableBuiltinPDFViewer = true; 
+      PDFjs = {
+        Enabled = false;
+        EnablePermissions = false;
+      };
+
+      # Extensions lockdown
       ExtensionUpdate = false;
-      # FIXME(Krey): Review `~/.mozilla/firefox/Default/extensions.json` and uninstall all unwanted
-      # Suggested by t0b0 thank you <3 https://gitlab.com/engmark/root/-/blob/60468eb82572d9a663b58498ce08fafbe545b808/configuration.nix#L293-310
-      # NOTE(Krey): Check if the addon is packaged on https://gitlab.com/rycee/nur-expressions/-/blob/master/pkgs/firefox-addons/addons.json
       ExtensionSettings = {
-        # "*" = {
-        #   installation_mode = "blocked";
-        #   blocked_install_message = "FUCKING FORGET IT!";
-        # };
+        "*" = {
+          installation_mode = "blocked";
+          blocked_install_message = "Nuh Uh";
+        };
         "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4493940/bitwarden_password_manager-2025.12.1.xpi";
           installation_mode = "force_installed";
@@ -98,252 +119,49 @@
         };
       };
 
+      # Not cool mozilla
       FirefoxHome = {
         Search = true;
         TopSites = false;
-        SponsoredTopSites = false; # Fuck you
+        SponsoredTopSites = false;
         Highlights = false;
         Pocket = false;
-        SponsoredPocket = false; # Fuck you
+        SponsoredPocket = false;
         Snippets = false;
         Locked = true;
       };
       FirefoxSuggest = {
         WebSuggestions = false;
-        SponsoredSuggestions = false; # Fuck you
+        SponsoredSuggestions = false;
         ImproveSuggest = false;
         Locked = true;
       };
-      Handlers = {
-        # FIXME-QA(Krey): Should be openned in evince if on GNOME
-        mimeTypes."application/pdf".action = "saveToDisk";
+
+      # Privacy settings
+      SearchEngines = {
+        PreventInstalls = true;
+        Default = "DuckDuckGo";
       };
-      extensions = {
-      };
-      NoDefaultBookmarks = true;
-      PasswordManagerEnabled = false; # Managed by KeepAss
-      PDFjs = {
-        Enabled = false; # Fuck No, HELL NO
-        EnablePermissions = false;
-      };
-      # Permissions = {
-      # 	Camera = {
-      # 		Allow = [https =//example.org,https =//example.org =1234];
-      # 		Block = [https =//example.edu];
-      # 		BlockNewRequests = true;
-      # 		Locked = true
-      # 	};
-      # 	Microphone = {
-      # 		Allow = [https =//example.org];
-      # 		Block = [https =//example.edu];
-      # 		BlockNewRequests = true;
-      # 		Locked = true
-      # 	};
-      # 	Location = {
-      # 		Allow = [https =//example.org];
-      # 		Block = [https =//example.edu];
-      # 		BlockNewRequests = true;
-      # 		Locked = true
-      # 	};
-      # 	Notifications = {
-      # 		Allow = [https =//example.org];
-      # 		Block = [https =//example.edu];
-      # 		BlockNewRequests = true;
-      # 		Locked = true
-      # 	};
-      # 	Autoplay = {
-      # 		Allow = [https =//example.org];
-      # 		Block = [https =//example.edu];
-      # 		Default = allow-audio-video | block-audio | block-audio-video;
-      # 		Locked = true
-      # 	};
-      # };
-      PictureInPicture = {
-        Enabled = true;
+      EnableTrackingProtection = {
+        Value = true;
         Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+        EmailTracking = true;
       };
       SanitizeOnShutdown = {
         Cache = true;
         Cookies = false;
         Downloads = true;
         FormData = true;
-        History = false;
+        History = true;
         Sessions = false;
-        SiteSettings = false;
+        SiteSettings = true;
         OfflineApps = true;
         Locked = true;
       };
-      SearchEngines = {
-        PreventInstalls = true;
-        Default = "DuckDuckGo";
-      };
-      SearchSuggestEnabled = false;
-      ShowHomeButton = true;
-      # FIXME-SECURITY(Krey): Decide what to do with this
-      # SSLVersionMax = tls1 | tls1.1 | tls1.2 | tls1.3;
-      # SSLVersionMin = tls1 | tls1.1 | tls1.2 | tls1.3;
-      # SupportMenu = {
-      # 	Title = Support Menu;
-      # 	URL = http =//example.com/support;
-      # 	AccessKey = S
-      # };
-      StartDownloadsInTempDirectory = true; # For speed? May fuck up the system on low ram
-      UserMessaging = {
-        ExtensionRecommendations = false; # Don’t recommend extensions while the user is visiting web pages
-        FeatureRecommendations = false; # Don’t recommend browser features
-        Locked = true; # Prevent the user from changing user messaging preferences
-        MoreFromMozilla = false; # Don’t show the “More from Mozilla” section in Preferences
-        SkipOnboarding = true; # Don’t show onboarding messages on the new tab page
-        UrlbarInterventions = false; # Don’t offer suggestions in the URL bar
-        WhatsNew = false; # Remove the “What’s New” icon and menuitem
-      };
-      UseSystemPrintDialog = true;
-      # WebsiteFilter = {
-      # 	Block = [<all_urls>];
-      # 	Exceptions = [http =//example.org/*]
-      # };
-    };
-    # arkenfox = {
-    # 	enable = false; # Decide how we want to handle these things
-    # 	version = "118.0"; # Used on 119.0, because we don't have firefox 118.0 handy
-    # };
 
-    profiles.Default = {
-      # settings = {
-      # # Enable letterboxing
-      # "privacy.resistFingerprinting.letterboxing" = true;
-
-      # # WebGL
-      # "webgl.disabled" = true;
-
-      # "browser.preferences.defaultPerformanceSettings.enabled" = false;
-      # "layers.acceleration.disabled" = true;
-      # "privacy.globalprivacycontrol.enabled" = true;
-
-      # "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-
-      # # "network.trr.mode" = 3;
-
-      # # "network.dns.disableIPv6" = false;
-
-      # "privacy.donottrackheader.enabled" = true;
-
-      # # "privacy.clearOnShutdown.history" = true;
-      # # "privacy.clearOnShutdown.downloads" = true;
-      # # "browser.sessionstore.resume_from_crash" = true;
-
-      # # See https://librewolf.net/docs/faq/#how-do-i-fully-prevent-autoplay for options
-      # "media.autoplay.blocking_policy" = 2;
-
-      # "privacy.resistFingerprinting" = true;
-      # };
-      # Documentation https://arkenfox.dwarfmaster.net
-      # arkenfox = {
-      #   enable = true;
-      #
-      #   # STARTUP
-      #   "0105".enable = true; # Disable sponsored content on Firefox Home (Activity Stream)
-      #
-      #   # GEOLOCATION / LANGUAGE / LOCALE
-      #   "0201".enable = true; # Use Mozilla geolocation service instead of Google if permission is granted [FF74+]
-      #   "0202".enable = true; # Disable using the OS's geolocation service
-      #   # WARNING(Krey): May break some input methods e.g xim/ibus for CJK languages [1]
-      #   "0211".enable = true; # Use en-US locale regardless of the system or region locale
-      #
-      #   # QUIETER FOX (Handles telemetry, etc..)
-      #   "0300".enable = true;
-      #
-      #   # BLOCK IMPLICIT OUTBOUND [not explicitly asked for - e.g. clicked on]
-      #   "0600".enable = true;
-      #
-      #   # DNS / DoH / PROXY / SOCKS / IPV6
-      #   "0701".enable = true; # Disable IPv6 as it's potentially leaky
-      #   "0704".enable = true; # Disable GIO as a potential proxy bypass vector
-      #
-      #   # LOCATION BAR / SEARCH BAR / SUGGESTIONS / HISTORY / FORMS
-      #   "0802".enable = true; # disable location bar domain guessing
-      #
-      #   # PASSWORDS
-      #   "0900".enable = true;
-      #
-      #   # HTTPS (SSL/TLS / OCSP / CERTS / HPKP)
-      #   "1200".enable = true;
-      #
-      #   # FONTS
-      #   "1400".enable = true;
-      #
-      #   # HEADERS ? REFERERS
-      #   "1600".enable = true;
-      #
-      #   # CONTAINERS
-      #   "1700".enable = true;
-      #
-      #   # PLUGINS / MEDIA / WEBRTC
-      #   "2002".enable = true; # Force WebRTC inside the proxy [FF70+]
-      #   "2003".enable = true; # Force a single network interface for ICE candidates generation [FF42+]
-      #   "2004".enable = true; # Force exclusion of private IPs from ICE candidates [FF51+]
-      #   "2020".enable = true; # Disable GMP (Gecko Media Plugins) - https://wiki.mozilla.org/GeckoMediaPlugins
-      #
-      #   # DOM (DOCUMENT OBJECT MODEL)
-      #   "2400".enable = true; # Prevent scrips from resizing open windows (could be used for fingerprinting)
-      #
-      #   # MISCELLANEOUS
-      #   "2603".enable = true; # Remove temp files opened with an external application on exit
-      #   "2606".enable = true; # Disable UITour backend so there is no chance that a remote page can use it
-      #   "2608".enable = true; # Reset remote debugging to disabled
-      #   "2615".enable = true; # Disable websites overriding Firefox's keyboard shortcuts [FF58+]
-      #   "2616".enable = true; # Remove special permissions for certain mozilla domains [FF35+]
-      #   "2617".enable = true; # Remove webchannel whitelist (Seems to be deprecated with mozilla having still permissions in it)
-      #   "2619".enable = true; # Use Punycode in Internationalized Domain Names to eliminate possible spoofing
-      #   "2620".enable = true; # Enforce PDFJS, disable PDFJS scripting
-      #   "2621".enable = true; # Disable links launching Windows Store on Windows 8/8.1/10 [WINDOWS]
-      #   "2623".enable = true; # Disable permissions delegation [FF73+], Disabling delegation means any prompts for these will show/use their correct 3rd party origin
-      #   "2624".enable = true; # Disable middle click on new tab button opening URLs or searches using clipboard [FF115+]
-      #   "2651".enable = true; # Enable user interaction for security by always asking where to download
-      #   "2652".enable = true; # Disable downloads panel opening on every download [FF96+]
-      #   "2654".enable = true; # Enable user interaction for security by always asking how to handle new mimetypes [FF101+]
-      #   "2662".enable = true; # Disable webextension restrictions on certain mozilla domains (you also need 4503) [FF60+]
-      #   "4503".enable = true; # Disable mozAddonManager Web API [FF57+]
-      #
-      #   # ETP (ENHANCED TRACKING PROTECTION)
-      #   "2700".enable = true;
-      #
-      #   "2811".enable = true; # Set/enforce what items to clear on shutdown (if 2810 is true) [SETUP-CHROME]
-      #   "2812".enable = true; # Set Session Restore to clear on shutdown (if 2810 is true) [FF34+]
-      #   "2815".enable = true; # Set "Cookies" and "Site Data" to clear on shutdown (if 2810 is true) [SETUP-CHROME]
-      #
-      #   # EFP (RESIST FINGERPRINTING)
-      #   "4500".enable = true;
-      #
-      #   # OPTIONAL OPSEC
-      #   "5003".enable = true; # Disable saving passwords
-      #   "5004".enable = true; # Disable permissions manager from writing to disk [FF41+] [RESTART], This means any permission changes are session only
-      #
-      #   # OPTIONAL HARDENING
-      #   ## NOTE(Krey): There are new vulnerabilities discovered in 2023, better disable it for now
-      #   "5505".enable = true; # Diable Ion and baseline JIT to harden against JS exploits
-      #
-      #   # DONT TOUTCH
-      #   ## NOTE(Krey): By default arkenfox flake sets all options are set to disabled, and these are expected to be always enabled
-      #   "6000".enable = true;
-      #
-      #   # DONT BOTHER
-      #   "7001".enable = true; # Disables Location-Aware Browsing, Full Screen Geo is behind a prompt (7002). Full screen requires user interaction
-      #   "7003".enable = true; # Disable non-modern cipher suites
-      #   "7004".enable = true; # Control TLS Versions, because they are used as a passive fingerprinting
-      #   "7005".enable = true; # Disable SSL Session IDs [FF36+]
-      #   "7006".enable = true; # Onions
-      #   "7007".enable = true; # Referencers, only cross-origin referers (1600s) need control
-      #   "7013".enable = true; # Disable Clipboard API
-      #   "7014".enable = true; # Disable System Add-on updates (Managed by Nix)
-      #
-      #   # NON-PROJECT RELATED
-      #   "9002".enable = true; # Disable General>Browsing>Recommend extensions/features as you browse [FF67+]
-      #   # FIXME-QA(Krey); Should just be a neutral state, dunno how to set it
-      #   settings = {
-      #     "network.proxy.socks_remote_dns" = true; # Do DNS lookup through proxy (required for tor to work)
-      #   };
-      # };
+      StartDownloadsInTempDirectory = true; # zoomies
     };
   };
 }
