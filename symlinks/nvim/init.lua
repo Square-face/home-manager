20751ca0f96df5984a862fffcf6e823c4ee1585f
Linus Michelsson <linus@sq8.dev>
2026-05-26 20:17:38 +0200

    fix(nvim): lazy.nvim -> vim.pack

--- symlinks/nvim/init.lua
+++ symlinks/nvim/init.lua
@@ -1,2 +1,20 @@
-require 'settings'
-require 'plugins'
+vim.pack.add({
+    'https://github.com/neovim-treesitter/nvim-treesitter',
+    'https://github.com/neovim-treesitter/treesitter-parser-registry',
+})
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "-"
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.swapfile = false
+vim.o.smartindent = true
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.o.winborder = "rounded"
+vim.o.completeopt = 'fuzzy,menuone,noselect,popup'
