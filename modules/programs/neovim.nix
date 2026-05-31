5ab460cf787705ba411af4bde9dd3ef27ed00b53
Linus Michelsson <linus@sq8.dev>
2026-05-31 21:31:23 +0200

    feat(neovim): removed treesitter plugin in favour of installing grammars using nix

--- modules/programs/neovim.nix
+++ modules/programs/neovim.nix
@@ -1,9 +1,37 @@
 {
   pkgs,
   config,
+  lib,
   linkDir,
   ...
 }:
+let 
+  grammars = with pkgs.vimPlugins.nvim-treesitter-parsers; [
+    lua
+    nix
+    zsh
+  ];
+
+  rtdir = lib.mkAfter (builtins.concatStringsSep "\n" (builtins.map (g: "vim.o.runtimepath = vim.o.runtimepath .. ',${g}'") grammars));
+
+  options = lib.mkBefore ''
+    vim.g.mapleader = " "
+    
+    vim.o.number = true
+    vim.o.relativenumber = true
+    
+    vim.o.swapfile = false
+    
+    -- Resonable tabs, fight me
+    vim.o.tabstop = 4
+    vim.o.shiftwidth = 4
+    vim.o.expandtab = true
+    vim.o.smartindent = true
+    
+    vim.o.winborder = "rounded"
+    vim.o.completeopt = 'fuzzy,menuone,noselect,popup'
+  '';
+in
 {
   programs.neovim = {
     enable = true;
@@ -12,8 +40,6 @@
     defaultEditor = true;
     withPython3 = false;
     withRuby = false;
+    initLua = lib.mkMerge [rtdir options];
   };
-
-  xdg.configFile.nvim.source = ../../symlinks/nvim;
-  xdg.configFile.nvim.recursive = true;
 }
